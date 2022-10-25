4dfb5baa8b1042a35807071f2e257e73ee783ec5
sin392 <sin392@gmail.com>
2022-10-25 11:47:35 +0900

    octomapの任意タイミングでの取得をサポート

---
+++ src/melodic/myrobot_moveit/src/octomap_handler.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python2
+# coding: UTF-8
+
+import rospy
+from sensor_msgs.msg import PointCloud2
+from std_srvs.srv import Empty
+
+# to achive octomap updating by custom timing
+class OctomapHandler:
+    def __init__(self, from_topics):
+        self.from_topics = from_topics
+        self.to_topics = [from_topic.replace("points", "republished_points") for from_topic in self.from_topics]
+        self.publishers = [rospy.Publisher(to_topic, PointCloud2, queue_size=5) for to_topic in self.to_topics]
+
+        rospy.wait_for_service('/clear_octomap')
+        self.clear_octomap_proxy = rospy.ServiceProxy('/clear_octomap', Empty)
+
+    def clear(self):
+        self.clear_octomap_proxy()
+
+    def update(self):
+        for from_topic, publisher in zip(self.from_topics, self.publishers):
+            msg = rospy.wait_for_message(from_topic, PointCloud2)
+            publisher.publish(msg)
+
+if __name__ == "__main__":
+    rospy.init_node("octomap_handler")
+
+    fps = rospy.get_param("fps", default=1)
+    ns = rospy.get_param("robot_name", default="myrobot")
+    sensors = rospy.get_param("~sensors", default=("body_camera", "left_camera", "right_camera"))
+
+    from_topics = ["/{}/{}/depth/color/points".format(ns, sensor_name) for sensor_name in sensors]
+
+    oh = OctomapHandler(from_topics)
+
+    rate = rospy.Rate(fps)
+
+    while not rospy.is_shutdown():
+        oh.update()
+        rate.sleep()
+    
